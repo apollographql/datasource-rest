dfb8bccce5e26247e7d5f9428eb1ca8d5dfaef08
Trevor Scheer <trevor.scheer@gmail.com>
2023-08-24 10:41:07 -0700

    Add optional `url` argument to `didEncounterErrors` hook (#242)
    In previous versions of RESTDataSource, the URL of the request was
    available on the Request object passed in to the hook. The Request
    object is no longer passed as an argument, so this restores the
    availability of the url to the hook.
    
    This is optional for now in order to keep this change forward compatible
    for existing this.didEncounterErrors call sites in userland code. In the
    next major version, this might become a required parameter.

---
+++ .changeset/gorgeous-timers-relax.md
@@ -0,0 +1,9 @@
+---
+'@apollo/datasource-rest': minor
+---
+
+Add optional `url` parameter to `didEncounterErrors` hook
+
+In previous versions of `RESTDataSource`, the URL of the request was available on the `Request` object passed in to the hook. The `Request` object is no longer passed as an argument, so this restores the availability of the `url` to the hook.
+
+This is optional for now in order to keep this change forward compatible for existing `this.didEncounterErrors` call sites in userland code. In the next major version, this might become a required parameter.
--- src/RESTDataSource.ts
+++ src/RESTDataSource.ts
@@ -261,7 +261,12 @@ export abstract class RESTDataSource {
     request: FetcherRequestInit,
   ): ValueOrPromise<CacheOptions | undefined>;
 
-  protected didEncounterError(_error: Error, _request: RequestOptions) {
+  protected didEncounterError(
+    _error: Error,
+    _request: RequestOptions,
+    // TODO(v7): this shouldn't be optional in a future major version
+    _url?: URL,
+  ) {
     // left as a no-op instead of an unimplemented optional method to avoid
     // breaking an existing use case where one calls
     // `super.didEncounterErrors(...)` This could be unimplemented / undefined
@@ -544,7 +549,7 @@ export abstract class RESTDataSource {
             },
           };
         } catch (error) {
-          this.didEncounterError(error as Error, outgoingRequest);
+          this.didEncounterError(error as Error, outgoingRequest, url);
           throw error;
         }
       });
